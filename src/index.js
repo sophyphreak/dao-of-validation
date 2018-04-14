9b2bb7732d89738ad74f161315e13f2c67269a1a
Andrew <arhorn@smcm.edu>
2018-04-14 16:57:10 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import doErrorsExist from './doErrorsExist/doErrorsExist';
+import amazonUrlDoesNotExist from './fieldSpecificValidators/amazonUrlDoesNotExist';
+import amazonUrlExistsButIsInvalid from './fieldSpecificValidators/amazonUrlExistsButIsInvalid';
+import authorBioDoesNotExist from './fieldSpecificValidators/authorBioDoesNotExist';
+import descriptionDoesNotExist from './fieldSpecificValidators/descriptionDoesNotExist';
+import emailDoesNotExist from './fieldSpecificValidators/emailDoesNotExist';
+import emailExistsButIsInvalid from './fieldSpecificValidators/emailExistsButIsInvalid';
+import fictionIsSelectedButGenreIsNotSelected from './fieldSpecificValidators/fictionIsSelectedButGenreIsNotSelected';
+import notesDoesNotExist from './fieldSpecificValidators/notesDoesNotExist';
+import priceDoesNotExist from './fieldSpecificValidators/priceDoesNotExist';
+import isValidPrice from './onChange/isValidPrice';
+
+const Validators = {
+  doErrorsExist,
+  amazonUrlDoesNotExist,
+  amazonUrlExistsButIsInvalid,
+  authorBioDoesNotExist,
+  descriptionDoesNotExist,
+  emailDoesNotExist,
+  emailExistsButIsInvalid,
+  fictionIsSelectedButGenreIsNotSelected,
+  notesDoesNotExist,
+  priceDoesNotExist,
+  isValidPrice
+};
+
+module.exports = Validators;
