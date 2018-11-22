0d5bf80e837242ac5d63902b357424156477da6d
Andrew <arhorn@smcm.edu>
2018-11-22 16:47:40 +0800

    removed dead code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import doErrorsExist from './doErrorsExist/doErrorsExist';
 import amazonUrlExistsButIsInvalid from './fieldSpecificValidators/amazonUrlExistsButIsInvalid';
-import authorBioDoesNotExist from './fieldSpecificValidators/authorBioDoesNotExist';
-import descriptionDoesNotExist from './fieldSpecificValidators/descriptionDoesNotExist';
 import emailExistsButIsInvalid from './fieldSpecificValidators/emailExistsButIsInvalid';
 import websiteUrlIsInvalid from './fieldSpecificValidators/websiteUrlIsInvalid';
 import thisIsEmpty from './general/thisIsEmpty';
